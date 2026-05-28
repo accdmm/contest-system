0ea17f3f1943f31df977d801b4bac36fcfbd79e0
zlfygydd <zlfygydd@user.noreply.gitee.com>
2026-05-29 00:16:32 +0800

    清理测试临时文件

--- start_minio.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-set MINIO_ROOT_USER=accdmm
-set MINIO_ROOT_PASSWORD=lmh050724
-start /B "" D:\Minio\bin\minio.exe server D:\develpo\minio\data --console-address "127.0.0.1:9000" --address "127.0.0.1:9005"
